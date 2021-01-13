50cb7a2baa72fda0fe4df99242c86ac63eec61d4
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-14 00:27:48 +0530

    fetch from db

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -2,11 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const[blogs , setBlogs] = useState([
-        { title: 'My new website' , body: 'lorem ipsum...' , author: 'mario' , id: 1},
-        { title: 'Welcome party!' , body: 'lorem ipsum...' , author: 'yoshi' , id: 2},
-        { title: 'Web dev top tips' , body: 'lorem ipsum...' , author: 'mario' , id: 3}
-    ]);
+    const[blogs , setBlogs] = useState([]);
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => {
             return blog.id !== id;
@@ -14,13 +10,14 @@ const Home = () => {
         setBlogs(newBlogs);
     };
     useEffect(() => {
-
-    });
+        fetch('http://localhost:8000/blogs')
+            .then((res) => res.json())
+            .then((data) => setBlogs(data))
+            .catch((error) => console.log(error.message));
+    }, []);
     return ( 
         <div className="home">
             <BlogList blogs = {blogs}  title="All Blogs!" handleDelete = {handleDelete} />
-            <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario')}  
-                title="Mario's Blogs" handleDelete = {handleDelete} />
         </div>
     );
 }
