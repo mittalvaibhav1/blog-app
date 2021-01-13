9ba2386a2de5406413259f1225ed751cf3a37749
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2021-01-14 00:55:40 +0530

    fetch error handling

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const[blogs , setBlogs] = useState([]);
+    const [blogs , setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => {
             return blog.id !== id;
@@ -12,15 +13,25 @@ const Home = () => {
     };
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then((res) => res.json())
+            .then((res) => {
+                if(!res.ok) {
+                    throw Error('Could not fetch Data');
+                }
+                return res.json();
+            })
             .then((data) => {
+                setError(null);
                 setBlogs(data);
                 setIsPending(false);
             })
-            .catch((error) => console.log(error.message));
+            .catch((error) => {
+                setError(error.message);
+                setIsPending(false);
+            });
     }, []);
     return ( 
         <div className="home">
+            {error && <div> { error } </div>}
             {isPending && <div>Loading..</div>}
             {blogs && <BlogList blogs = {blogs}  title="All Blogs!" handleDelete = {handleDelete} /> }
         </div>
